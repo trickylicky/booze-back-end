c8ea3bff9289cef0295d75b201d1b6172a364771
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-09 13:07:48 +0300

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_09_085849) do
+
+  create_table "liquors", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.integer "rating"
+    t.string "category"
+    t.string "description"
+    t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
