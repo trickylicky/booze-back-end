734d64179e6afd9739aa516cb6c87490f8f11172
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-09 11:02:08 +0300

    Updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,17 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+# product.create(product_name: 'SpaceX')
+# response = RestClient.get ' https://liqour-store.herokuapp.com'
+# products = JSON.parse(response)
+# products.each do |product|
+#    new_launch = Launch.create(
+#         name: product['name'],
+#         image: product['image'],
+#         rating: product['rating'],
+#         category: product['category']
+#         description: product['description'],
+#         company_id: 1,
+#     )
+# end
 puts "✅ Done seeding!"
