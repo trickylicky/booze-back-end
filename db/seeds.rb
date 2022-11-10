46743c9db281538fff834cab8707e8934cd03ba8
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-10 10:42:02 +0300

    Changed variable names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 puts "🌱 Seeding spices..."
 
-liqour.create(liquor_name: 'liquor')
+
 response = RestClient.get 'https://liquor-data.herokuapp.com/liquor'
 liquor = JSON.parse(response)
-liqour.each do |liqour|
-   new_liquor = Launch.create(
-        name: liqour['name'],
-        image: liqour['image'],
-        rating: liqour['rating'],
-        category: liqour['category']
-        description: liqour['description'],
+liquor.each do |liquor|
+   new_liquor = Liquor.create(
+        title: liquor['title'],
+        image_url: liquor['image'],
+        rating: liquor['rating'],
+        category: liquor['category'],
+        description: liquor['description'],
     )
 end
 puts "✅ Done seeding!"
