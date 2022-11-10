d5534757a9df01b7d5697178c9e3349c8c6cd90c
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-10 10:50:56 +0300

    removed rating from liquors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_062121) do
+ActiveRecord::Schema.define(version: 2022_11_10_074612) do
 
   create_table "liquors", force: :cascade do |t|
     t.string "title"
     t.string "image_url"
-    t.integer "rating"
     t.string "category"
     t.string "description"
     t.integer "price"
